3d7edb1290092779c46e00459d2cad8038df3755
ericxlima <eric.vinlima@gmail.com>
2023-12-14 05:19:00 -0300

    fix: move logic from projects section to js file

--- main.js
+++ main.js
@@ -28,3 +28,72 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Erro ao carregar dados do JSON:', error));
 });
+
+
+// main.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('data/projects.json')
+      .then(response => response.json())
+      .then(data => {
+        const projectsSection = document.getElementById('projects');
+  
+        data.forEach(project => {
+          const cardCol = document.createElement('div');
+          cardCol.classList.add('col');
+  
+          const card = document.createElement('div');
+          card.classList.add('card', 'h-100');
+  
+          const img = document.createElement('img');
+          img.classList.add('card-img-top');
+          img.src = project.image;
+          img.alt = project.title;
+  
+          const cardBody = document.createElement('div');
+          cardBody.classList.add('card-body');
+  
+          const title = document.createElement('h5');
+          title.classList.add('card-title');
+          title.textContent = project.title;
+  
+          const description = document.createElement('p');
+          description.classList.add('card-text');
+          description.textContent = project.description;
+  
+          const linksDiv = document.createElement('div');
+          linksDiv.classList.add('d-flex');
+  
+          const githubLink = document.createElement('a');
+          githubLink.classList.add('me-3', 'py-2', 'link-body-emphasis', 'text-decoration-none');
+          githubLink.href = project.githubLink;
+          githubLink.target = '_blank'
+  
+          const githubImg = document.createElement('img');
+          githubImg.classList.add('card-github-image');
+          githubImg.src = 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg';
+  
+          const viewCodeLink = document.createElement('a');
+          viewCodeLink.classList.add('card-github-link');
+          viewCodeLink.href = project.githubLink;
+          viewCodeLink.textContent = 'View Code';
+          viewCodeLink.target = '_blank'
+  
+          // Montar a estrutura do cartão
+          githubLink.appendChild(githubImg);
+          linksDiv.appendChild(githubLink);
+          linksDiv.appendChild(viewCodeLink);
+          cardBody.appendChild(title);
+          cardBody.appendChild(description);
+          cardBody.appendChild(linksDiv);
+          card.appendChild(img);
+          card.appendChild(cardBody);
+          cardCol.appendChild(card);
+  
+          // Adicionar o cartão à seção de projetos
+          projectsSection.querySelector('.row').appendChild(cardCol);
+        });
+      })
+      .catch(error => console.error('Erro ao carregar dados do JSON:', error));
+  });
+  
