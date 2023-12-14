6a6bb3312752c37b538164b8cdca56e38f279977
ericxlima <eric.vinlima@gmail.com>
2023-12-14 06:37:35 -0300

    fix: add responsive carousel size

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
     fetch('data/stacks.json')
-        .then(response => response.json())
-        .then(data => {
-            const stackCarousel = document.getElementById('stackCarousel');
-
-            data.forEach((stack, index) => {
-                const carouselItem = document.createElement('div');
-                carouselItem.classList.add('carousel-item');
-                if (index === 0) {
-                    carouselItem.classList.add('active');
-                }
-
-                const imageContainer = document.createElement('div');
-                imageContainer.classList.add('d-flex', 'justify-content-center');
-
-                stack.forEach(stackItem => {
-                    const img = document.createElement('img');
-                    img.classList.add('stack-image', 'img-fluid');
-                    img.src = stackItem.image;
-                    img.alt = stackItem.language;
-                    imageContainer.appendChild(img);
-                });
-
-                carouselItem.appendChild(imageContainer);
-                stackCarousel.querySelector('.carousel-inner').appendChild(carouselItem);
-            });
-        })
-        .catch(error => console.error('Erro ao carregar dados do JSON:', error));
-});
+      .then(response => response.json())
+      .then(data => {
+        const stackCarousel = document.getElementById('stackCarousel');
+        const carouselInner = stackCarousel.querySelector('.carousel-inner');
+  
+        const screenWidth = window.innerWidth;
+  
+        let numberOfImages;
+        if (screenWidth >= 1200) {
+          numberOfImages = 7; // Desktop
+        } else if (screenWidth >= 768) {
+          numberOfImages = 5; // Tablet
+        } else {
+          numberOfImages = 3; // Celular
+        }
+  
+        let currentIndex = 0;
+        for (let i = 0; i < data.length; i += numberOfImages) {
+          const carouselItem = document.createElement('div');
+          carouselItem.classList.add('carousel-item');
+          if (currentIndex === 0) {
+            carouselItem.classList.add('active');
+          }
+  
+          const imageContainer = document.createElement('div');
+          imageContainer.classList.add('d-flex', 'justify-content-center');
+  
+          for (let j = 0; j < numberOfImages && currentIndex < data.length; j++) {
+            const img = document.createElement('img');
+            img.classList.add('stack-image', 'img-fluid');
+            img.src = data[currentIndex].image;
+            img.alt = data[currentIndex].language;
+            imageContainer.appendChild(img);
+            currentIndex++;
+          }
+  
+          carouselItem.appendChild(imageContainer);
+          carouselInner.appendChild(carouselItem);
+        }
+      })
+      .catch(error => console.error('Erro ao carregar dados do JSON:', error));
+  });
+  
 
 
 // main.js
